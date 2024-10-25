1800ddaa58556bd3340a513201af003100ca54d5
EnzoSally <myenzo121@gmail.com>
2024-10-24 21:15:48 -0300

    enviando microservices para github

---
+++ api-geteway-service/src/main/java/br/edu/atitus/paradigma/api_gateway_service/ApiGetewayServiceApplication.java
@@ -0,0 +1,13 @@
+package br.edu.atitus.paradigma.api_gateway_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiGetewayServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGetewayServiceApplication.class, args);
+	}
+
+}
---
+++ api-geteway-service/src/main/java/br/edu/atitus/paradigma/api_gateway_service/configs/ApiGatewayConfig.java
@@ -0,0 +1,43 @@
+package br.edu.atitus.paradigma.api_gateway_service.configs;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ApiGatewayConfig {
+
+	@Bean
+	RouteLocator getRoutes(RouteLocatorBuilder builder) {
+	    return builder.routes()
+	        .route(rota -> rota
+	                .path("/get")
+	                .filters(f -> f
+	                        .addRequestHeader("Usuario", "Fulano")
+	                        .addRequestHeader("Usuario", "Enzo Almeida")
+	                        .addRequestParameter("Parametro", "ValorParametro")
+	                        .addResponseHeader("Servidor", "meuServer"))
+	                .uri("http://httpbin.org"))
+	        
+	        .route(rota -> rota
+	                .path("/cambio-service/**")
+	                .filters(f -> f
+	                        .addRequestHeader("Usuario", "Enzo Almeida"))
+	                .uri("lb://cambio-service"))
+	        
+	        .route(rota -> rota
+	                .path("/produto-service/**")
+	                .filters(f -> f
+	                        .addRequestHeader("Usuario", "Enzo Almeida"))
+	                .uri("lb://produto-service"))
+	        
+	        .route(rota -> rota
+	                .path("/saudacao-service/**")
+	                .filters(f -> f
+	                        .addRequestHeader("Usuario", "Enzo Almeida"))
+	                .uri("lb://saudacao-service"))
+	        .build();
+	}
+
+}
